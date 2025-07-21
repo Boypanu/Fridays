54d602fa03b8c9967cf8e530bc4e7ba0c1cc634c
Boypanu <boypanu6907@example.com>
2025-07-21 18:56:03 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   api1/app.py
    	modified:   api2/app.py

--- api1/app.py
+++ api1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 import logging
 
@@ -7,21 +7,26 @@ app = Flask(__name__)
 # Setup logging
 logging.basicConfig(level=logging.INFO)
 
-API2_URL = "http://api2:5001/api2"
+API2_URL = "http://api_2:5001/api2"
 
 @app.route("/api_1", methods=["GET"])
 def call_api2():
-    app.logger.info("API1: Received request from user")
+    app.logger.info("API1: Received GET request from user")
+    # Example JSON payload
+    payload = {
+        "name": "API1",
+        "message": "Hello API2 from API1"
+    }
     try:
-        response = requests.get(API2_URL)
-        app.logger.info("API1: Called API2 successfully")
+        response = requests.post(API2_URL, json=payload, timeout=5)
+        app.logger.info("API1: Sent POST to API2 successfully")
         return jsonify({
-            "api1_message": "Hello from API_1 ",
+            "api1_message": "Hello from API1",
             "api2_response": response.json()
         })
     except requests.exceptions.RequestException as e:
         app.logger.error(f"API1: Error calling API2: {e}")
-        return jsonify({"error": "API2 is unreachable"}), 500
+        return jsonify({"error": "API2 unreachable"}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- api2/app.py
+++ api2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import logging
 
 app = Flask(__name__)
@@ -6,10 +6,23 @@ app = Flask(__name__)
 # Setup logging
 logging.basicConfig(level=logging.INFO)
 
-@app.route("/api2", methods=["GET"])
+@app.route("/api2", methods=["GET", "POST"])
 def api2_response():
-    app.logger.info("API2: Received request from API1")
-    return jsonify({"api2_message": "Hello from API_2"})
+    if request.method == "GET":
+        app.logger.info("API2: Received GET request from API1")
+        return jsonify({"api2_message": "Hello from API_2 (GET)"})
+    
+    if request.method == "POST":
+        data = request.get_json()
+        app.logger.info(f"API2: Received POST data: {data}")
+        return jsonify({
+            "api2_message": "Hello from API_2 (POST)",
+            "received_data": data
+        })
+
+@app.route("/health", methods=["GET"])
+def health():
+    return {"status": "ok"}, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
